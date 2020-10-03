e67806d5b00b00497eb4fe46e59b4bacf724ddf5
Skwangles <skwanglesavenue@gmail.com>
2020-10-03 15:29:45 +1300

    Add project files.

---
+++ VotingProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VotingProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
