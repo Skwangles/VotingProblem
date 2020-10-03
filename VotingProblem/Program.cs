a02e6a63080e0d45008d83b864fd7b460294f2b5
Skwangles <skwanglesavenue@gmail.com>
2020-10-03 17:49:42 +1300

    Realised strings could natively be indexed. Realised the read from file gave an array off the bat. Made a voter class, which counts its own number, and checks if it is still in the race(Or has been eliminated)
    ToDp:
    Will program in code to remove itself(Voter Class' number) from the array and append "0".

--- VotingProblem/Program.cs
+++ VotingProblem/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace VotingProblem
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var one = new Voter(1);
+            var two = new Voter(2);
+            var three = new Voter(3);
+            var four = new Voter(4);
+            var five = new Voter(5);
+
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\tree_\Downloads\Votes.txt");
+            /*
+            string[,] voteRecord = new string[lines.Length];
+            int counter = 0;
+            foreach (string line in lines)
+            {
+                var temp = line.Split("");
+                int increment = 0;
+                foreach (string num in temp)
+                {
+                    voteRecord[counter, increment] = num;
+                    increment++;
+                }
+                counter++;
+
+            }
+            */
+            Display("First Preferences:");
+            FindLeast();
+
+
+            void FindLeast()
+            {
+                var voteCount = new int[,]
+                {
+                    {int.Parse(one.Count(voteRecord)), 1 },
+                    { int.Parse(two.Count(voteRecord)), 2 },
+                    { int.Parse(three.Count(voteRecord)), 3 },
+                    { int.Parse(four.Count(voteRecord)), 4 },
+                    { int.Parse(five.Count(voteRecord)), 5 }
+                };
+
+            }
+
+
+            //functions
+            void Display(string Status)
+            {
+                Console.WriteLine(Status);
+                Console.WriteLine("Candidate 1: " + one.Count(voteRecord).ToString());
+                Console.WriteLine("Candidate 2: " + two.Count(voteRecord).ToString());
+                Console.WriteLine("Candidate 3: " + three.Count(voteRecord).ToString());
+                Console.WriteLine("Candidate 4: " + four.Count(voteRecord).ToString());
+                Console.WriteLine("Candidate 5: " + five.Count(voteRecord).ToString());
+
+            }
         }
     }
 }
---
+++ VotingProblem/Voter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VotingProblem
+{
+    class Voter
+    {
+        private bool isInRace;
+        int candidateNumber;
+        public Voter( int number)
+        {
+            isInRace = true;//to start the candidate is always in the race
+            this.candidateNumber = number;//tells class what number to search for
+        }
+        public string Count(string[] votes)//counts the amount of its numbers in first position, should be 0 when eliminated
+        {
+            int counted = 0;//resets count to 0 always
+            for(int inc = 0; inc < votes.Length; inc++)
+            {
+                if(int.Parse(votes[inc][0]) == candidateNumber)
+                {
+                    counted++;
+                }
+
+            }
+
+            if (isInRace)//checks if the candidate has been eliminated from the race or not. 
+            {
+                return counted.ToString();
+            }
+            else
+            {
+                return "Eliminated";
+            }
+            
+        }
+
+        public string[,] Eliminate(string[] vote)
+        {
+            //here remove the Candidate's number and shift all others to the left. 
+
+            return vote;
+        }
+
+
+
+    }
+}
